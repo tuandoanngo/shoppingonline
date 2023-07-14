f19ec1783afa63e79e842048f46830a0a4a5f45e
tuandoanngo <117096061+tuandoanngo@users.noreply.github.com>
2023-07-14 09:52:39 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shoppingonline.mishl0c.mongodb.net',
+    DB_USER: 'admin',
+    DB_PASS: '147123159',
+    DB_DATABASE: 'ShoppingOnline',
+    EMAIL_USER: 'tuandoanngo255@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'ooecdvzhrtxaxgeo',
+    JWT_SECRET: '147123159',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
